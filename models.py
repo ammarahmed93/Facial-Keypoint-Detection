5715f0167ed2bca3217adfe7d71fdee37b2e7b4b
Ammar Ahmed <ammarahmed@v1020-wn-9-5.campus-dynamic.uwaterloo.ca>
2020-08-11 21:29:11 -0500

    modified parts of the prompt

--- models.py
+++ models.py
@@ -11,13 +11,13 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         
-        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## Define all the layers of this CNN, the only requirements are:
         ## 1. This network takes in a square (same width and height), grayscale image as input
         ## 2. It ends with a linear layer that represents the keypoints
         ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
-        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 3x3 square convolution kernel
         ## output size = (W-F)/S +1 = (224-3)/1 +1 = 222
         # the output Tensor for one image, will have the dimensions: (16, 222, 222)
         # after one pool layer, the output Tensor for one image will become (16, 111, 111)
@@ -28,21 +28,21 @@ class Net(nn.Module):
         self.pool = nn.MaxPool2d(2, 2)
         
         # Define a second conv layer
-        # 32 input image channel (grayscale), 64 output channels/feature maps, 5x5 square convolution kernel
+        # 32 input image channel (grayscale), 64 output channels/feature maps, 3x3 square convolution kernel
         ## output size = (W-F)/S +1 = (111-3)/1 + 1 = 109
         # the output Tensor for one image, will have the dimensions: (32, 109, 109)
         # after one pool layer, the output Tensor for one image will become (32, 54, 54) $ not used 
         self.conv2 = nn.Conv2d(16, 32, 3)
         self.conv2_bn = nn.BatchNorm2d(32)
         # Define a 3rd conv layer
-        # 64 input image channel (grayscale), 128 output channels/feature maps, 5x5 square convolution kernel
+        # 64 input image channel (grayscale), 128 output channels/feature maps, 3x3 square convolution kernel
         ## output size = (W-F)/S +1 = (54-3)/1 +1 = 52
         # the output Tensor for one image, will have the dimensions: (64, 52, 52)
         # after one pool layer, the output Tensor for one image will become (64, 26, 26)
         self.conv3 = nn.Conv2d(32, 64, 3)
         self.conv3_bn = nn.BatchNorm2d(64)
         # Define a 4th conv layer
-        # 64 input image channel (grayscale), 256 output channels/feature maps, 5x5 square convolution kernel
+        # 64 input image channel (grayscale), 256 output channels/feature maps, 3x3 square convolution kernel
         ## output size = (W-F)/S +1 = (26-3)/1 +1 = 24
         # the output Tensor for one image, will have the dimensions: (128, 24, 24)
         # after one pool layer, the output Tensor for one image will become (128, 12, 12)
@@ -50,7 +50,7 @@ class Net(nn.Module):
         self.conv4_bn = nn.BatchNorm2d(128)
         
         # Define a 5th conv layer
-        # 64 input image channel (grayscale), 256 output channels/feature maps, 5x5 square convolution kernel
+        # 64 input image channel (grayscale), 256 output channels/feature maps, 3x3 square convolution kernel
         ## output size = (W-F)/S +1 = (12-3)/1 +1 = 10
         # the output Tensor for one image, will have the dimensions: (256, 10, 10)
         # after one pool layer, the output Tensor for one image will become (256, 5, 5)
@@ -66,10 +66,8 @@ class Net(nn.Module):
         # finally, create 136 output channels (for the 136 classes)
         self.fc2 = nn.Linear(512, 136)
         
-        ## Note that among the layers to add, consider including:
-        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
     def forward(self, x):
-        ## TODO: Define the feedforward behavior of this model
+        ## Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         x = self.pool(F.relu(self.conv1_bn(self.conv1(x))))
         x = self.pool(F.relu(self.conv2_bn(self.conv2(x))))
@@ -84,7 +82,7 @@ class Net(nn.Module):
         x = self.fc1_drop(x)
         x = self.fc2(x)
     
-        # a modified x, having gone through all the layers of your model, should be returned
+        # a modified x, having gone through all the layers of the model, should be returned
         return x
     
     
